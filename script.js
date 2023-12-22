f28b8cc01448eacb908cfca9d4748a8d7e80fd24
dostoynikov <145872991+dostoynikov@users.noreply.github.com>
2023-12-22 21:56:12 +0200

    implement numbers with values and a click function

--- script.js
+++ script.js
@@ -5,22 +5,36 @@ let add = '+';
 let subtract = '-';
 let multiply = '*';
 let divide = '/';
+let displayValue;
 
-function operate (op, num1, num2) {
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        console.log(e.target.dataset.value)
+
+    })
+});
+
+
+function operate(op, num1, num2) {
     switch (op) {
         case add:
-            return num1+num2;
+            return num1 + num2;
             break;
         case subtract:
-            return num1-num2;
+            return num1 - num2;
             break;
         case multiply:
-            return num1*num2;
+            return num1 * num2;
             break;
         case divide:
-            return num1/num2;
+            return num1 / num2;
             break;
     }
 
 };
+
+
 
